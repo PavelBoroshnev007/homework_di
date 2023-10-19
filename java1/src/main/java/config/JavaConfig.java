76f26eb60d7f21c6f8e3895c5761eacb34dd29f3
Pavel Boroshnev <pavelboroshnev9@gmail.com>
2023-10-20 00:21:40 +0300

    add file

---
+++ java1/src/main/java/config/JavaConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import controller.PostController;
+import repository.PostRepository;
+import repository.PostRepositoryStubImpl;
+import service.PostService;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    // аргумент метода и есть DI
+    // название метода - название бина
+    public PostController postController(PostService service) {
+        return new PostController(service);
+    }
+
+    @Bean
+    public PostService postService(PostRepository repository) {
+        return new PostService(repository);
+    }
+
+    @Bean
+    public PostRepository postRepository() {
+        return new PostRepositoryStubImpl();
+    }
+}
